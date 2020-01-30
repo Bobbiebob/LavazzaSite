1ec0e083e3af98440623d0dc59aa33322a5e4b07
Rick Bakker <rickbakkr@gmail.com>
2020-01-30 14:37:05 +0100

    Leaflet map + loader (better than broken page :p)

--- application/Controllers/DashboardController.php
+++ application/Controllers/DashboardController.php
@@ -43,19 +43,116 @@ class DashboardController extends BaseController
             ->table('stations');
         $db->whereOperator(DB::OR);
 
+        $points = [
+            // Norway
+            10260, // Trondo
+            13840, // Oslo
+            13170, // Bergen
+            11520,// bodo
+            12710, // Trondheim
+            12230, // Kristiansund
+            11220, // Mosjoen
+            13100, // Floro
+            14520, // Kristiandand
+            14150, // Stavanger
+            14825, // Torp
+
+            // Sweden
+            24640, // Stockholm
+            20730, // Karlstad
+            21860, // Lulea
+            20660, // Sundsvall,
+            20440, // Kiruna
+            22260, // Ostersund
+            21190, // Umea
+            20130, // RItsem
+            20240, // Stora
+            25130, // goteborg
+            26110, // Helsingborg
+            26720, // Kalmar
+            20580, // Harapanda
+            22300, // Gubbhogen
+            23160, // Sarna
+            24350, // Borlange
+            25920, // Olands
+            20900, // Visby
+            20870, // Jonkoping
+            26350, // Malmo
+
+            // Denmark
+            60300, // Aalborg
+            60120, // Fugloy
+            60740, // Aarhus
+            61040, // Billund
+            61800, // Koebenhaven
+            60890, // Seadenstrnad
+            60240, // Thisted
+            61200, // Odense
+            60800, // Esbjerg
+
+            // Netherlands
+            62790, // Hoogeveen
+            62800, // Groningen
+            63440, // Rotterdam
+            62400, // Amsterdam
+            62900, // Twente
+            62750, // Deelden
+            62600, // De bilt
+            63250, // Zierikzee
+            63750, // volkel
+            63700, // eindhoven
+
+            // BElgium
+            63700, // Oostende
+            64500, // Antwerp
+            64310, // Gent
+            64000, // Koksijde,
+            64510, // Bruxelles,
+            64785, // Liege,
+            64960, // Elsenborn
+            64780, // bierset
+            64490, // Charleroi
+            64900, // Spa
+            64760, // St Hubert
+
+            // LUX
+            64785,
+            65900,
+
+            // Switzerland
+
+            66200, // SCHAFFHAUSEN
+            66700, // ZURICH-KLOTEN
+            66600, // ZUERICH METEOSCHWEI
+            66621, // GRENCHEN
+            66300, // BERN-BELP
+            66100, // PAYERNE
+            66304, // SAMEDAN
+            67000, // GENEVE-COINTRIN
+            67200, // SION
+        ];
+
+        foreach($points as $point) {
+            $db->where('stn', '=', $point);
+        }
+
+        /* Countries having relatively few monitors, so manual sampling is not required */
+        $db->where('country', '=', 'ESTONIA');
+        $db->where('country', '=', 'LATVIA');
+        $db->where('country', '=', 'LITHUANIA');
+        $db->where('country', '=', 'BELARUS');
+        $db->where('country', '=', 'MOLDOVA');
+        $db->where('country', '=', 'BULGARIA');
+        $db->where('country', '=', 'CROATIA');
+        $db->where('country', '=', 'POLAND');
+
         $db->where('stn', '=', '10080');
-        /*
-         *     cy: 459;
-    cx: 255;
-         */
         $db->where('stn', '=', '160590');
         $db->where('stn', '=', '103130');
         $db->where('stn', '=', '156091');
 
         $db->where('stn', '=', '80001');
         $db->where('stn', '=', '85600');
-
-//        $db->where('stn', '=', '85750');
 
         $db->run();
         $stations = $db->fetchAll();
