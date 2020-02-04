e9ea259340873ba2dfa61cfa5c9d8471265da970
Allardos <45425325+Bobbiebob@users.noreply.github.com>
2020-02-04 11:28:55 +0100

    icons added colombia

--- application/Controllers/ParserController.php
+++ application/Controllers/ParserController.php
@@ -3,11 +3,16 @@
 
 namespace Application\Controllers;
 
+use Application\Helpers\Config;
 use Application\Helpers\Parser;
 
 class ParserController
 {
     public function getView(){
-        (Parser::readString());
+        // full path to text file
+        define("TEXT_FILE", Config::get('stationFiles.path').'station');
+        // number of lines to read from the end of file
+        define("LINES_COUNT", Config::get('stationFiles.lines'));
+        (Parser::readString(TEXT_FILE, LINES_COUNT));
     }
 }
